37e084349d25f4b101e5379c0ca8fd435ddcb4a2
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2024-11-28 23:34:54 -0500

    Integración Crear tema y asociar a un curso

--- src/main/java/com/milenyumsoft/controldecursos/controller/CursoController.java
+++ src/main/java/com/milenyumsoft/controldecursos/controller/CursoController.java
@@ -3,9 +3,7 @@ package com.milenyumsoft.controldecursos.controller;
 import com.milenyumsoft.controldecursos.model.Curso;
 import com.milenyumsoft.controldecursos.service.ICursoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CursoController {
@@ -21,6 +19,14 @@ public class CursoController {
         cursoService.createCurso(curso);
         return "Curso creado, exitosamente";
     }
+
+    @PutMapping("curso/editar")
+    public Curso editCurso(@RequestBody Curso curso) {
+
+        Curso cur = cursoService.updateCurso(curso);
+
+        return cur;
+            }
 
 
 }
--- src/main/java/com/milenyumsoft/controldecursos/controller/TemaController.java
+++ src/main/java/com/milenyumsoft/controldecursos/controller/TemaController.java
@@ -1,13 +1,23 @@
 package com.milenyumsoft.controldecursos.controller;
 
+import com.milenyumsoft.controldecursos.model.Tema;
+import com.milenyumsoft.controldecursos.service.ITemaService;
 import com.milenyumsoft.controldecursos.service.TemaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TemaController {
     @Autowired
-    private TemaService temaService;
+    private ITemaService temaService;
 
 
+    //Crear tema
+    @PostMapping("tema/crear")
+    public String crearTema(@RequestBody Tema tema) {
+        temaService.saveTema(tema);
+        return "Tema creada, exitosamente";
+    }
 }
